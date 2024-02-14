5d1c7a1b892f4e35af86ebfba2f02d3174d0b1a2
Ray <rayc12079@gmail.com>
2024-02-14 00:05:46 -0500

    Responsive Navigation Menu

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from "../logo.png"
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function NavigationBar() {
+  const [dropdownVisible, setDropdownVisible] = useState(false)
   return (
-    <nav className='bg-gray-100 h-16 m-4 rounded-2xl drop-shadow-md flex items-center p-4 justify-between'>
-      <img src={Logo} alt="logo" height="35px" width="35px" className='cursor-pointer'/>
-      <ul className='hidden list-none items-center gap-6 sm:flex'>
+    <nav className={`bg-gray-100 my-6 mx-4 rounded-2xl drop-shadow-md flex items-center p-6 justify-between gap-6 ${dropdownVisible && "flex-col"}`}>
+      <section className='flex justify-between items-center w-full'>
+        <img src={Logo} alt="logo" height="35px" width="35px" className='cursor-pointer'/>
+        <FontAwesomeIcon icon={faBars} className='block sm:hidden text-xl' onClick={() => setDropdownVisible(prev => !prev)}/>
+      </section>
+      <ul className={`list-none items-center gap-6 sm:flex ${dropdownVisible ? "flex flex-col" : "hidden flex-row"}`}>
         <a href="#greeting">
           <li className='cursor-pointer font-medium'>Home</li>
         </a>
