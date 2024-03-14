e579e6b3cbfc8804d53bfcf307df6713b43f6ec2
Ray <rayc12079@gmail.com>
2024-03-13 21:20:17 -0400

    Updated Resume and added Next.js to skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,15 +7,16 @@ export default function Skills() {
       <h1 className='text-2xl font-bold'>Technologies:</h1>
       <br />
       <div className='flex flex-wrap items-center gap-4 justify-center'>
-        <img src="./assets/html.png" alt="HTML" className='drop-shadow-md cursor-pointer'/>
-        <img src="./assets/css.png" alt="CSS" className='drop-shadow-md cursor-pointer'/>
-        <img src="./assets/javascript.png" alt="JAVASCRIPT" className='drop-shadow-md cursor-pointer'/>
-        <img src="./assets/typescript.png" alt="TYPESCRIPT" className='drop-shadow-md cursor-pointer'/>
-        <img src="./assets/react.png" alt="REACT" className='drop-shadow-md cursor-pointer'/>
-        <img src="./assets/reactRouter.png" alt="REACT ROUTER" className='drop-shadow-md cursor-pointer' />
-        <img src="/assets/firebase.png" alt="FIREBASE" className='drop-shadow-md cursor-pointer'/>
-        <img src="./assets/redux.png" alt="REDUX" className='drop-shadow-md cursor-pointer'/>
-        <img src="./assets/tailwind.png" alt="TAILWIND" className='drop-shadow-md cursor-pointer'/>
+        <img src="./assets/html.png" alt="HTML" className='drop-shadow-md cursor-pointer' title='HTML'/>
+        <img src="./assets/css.png" alt="CSS" className='drop-shadow-md cursor-pointer' title='CSS'/>
+        <img src="./assets/javascript.png" alt="JAVASCRIPT" className='drop-shadow-md cursor-pointer' title='Javascript'/>
+        <img src="./assets/typescript.png" alt="TYPESCRIPT" className='drop-shadow-md cursor-pointer' title='Typescript'/>
+        <img src="./assets/react.png" alt="REACT" className='drop-shadow-md cursor-pointer' title='React.js'/>
+        <img src="./assets/reactRouter.png" alt="REACT ROUTER" className='drop-shadow-md cursor-pointer' title='React Router'/>
+        <img src="./assets/next.png" alt="NEXT" className='drop-shadow-md cursor-pointer' title='Next.js'/>
+        <img src="/assets/firebase.png" alt="FIREBASE" className='drop-shadow-md cursor-pointer' title='Firebase'/>
+        <img src="./assets/redux.png" alt="REDUX" className='drop-shadow-md cursor-pointer' title='Redux'/>
+        <img src="./assets/tailwind.png" alt="TAILWIND" className='drop-shadow-md cursor-pointer' title='Tailwind'/>
       </div>
     </motion.section>
   )
